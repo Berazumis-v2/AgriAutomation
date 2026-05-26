fc03871cdd8ef96d0886ee8d5347a7d1bb8342e2
Berazumis-v2 <122414446+Berazumis-v2@users.noreply.github.com>
2026-05-26 23:54:38 +0300

    fix: change rendeMode

--- client/Agri/src/app/app.routes.server.ts
+++ client/Agri/src/app/app.routes.server.ts
@@ -1,8 +1,11 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+// Disable global prerendering for all routes because many routes include
+// parameters (e.g. ':id') and require `getPrerenderParams` to prerender.
+// Use client rendering for parameterized routes to avoid build-time errors.
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client
   }
 ];
